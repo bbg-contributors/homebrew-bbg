8981d0c44995b4f529d492d35c064795e3dea8fe
Misaka13514 <Misaka13514@gmail.com>
2022-08-29 20:53:23 +0800

    fix according to brew cask cookbook

--- Casks/bbg.rb
+++ Casks/bbg.rb
@@ -2,20 +2,18 @@ cask "bbg" do
   arch arm: "arm64", intel: "x64"
 
   version "20220817"
-  sha256 :no_check
+  sha256 arm:   "ee4b6e0abeec1e90d90c4e8b97895c84ec2c8df641e5eeaa8cbe4234871b1f15",
+         intel: "091ac8f14e86083c666ed6e839a089abd3d471d5096e59bbdfef5f8a538afd89"
 
-  url "https://github.com/bbg-contributors/bbg/releases/download/#{version}/bbg-#{version}-#{arch}.dmg"
+  url "https://github.com/bbg-contributors/bbg/releases/download/#{version}/bbg-#{version}-#{arch}.dmg",
+      verified: "https://github.com/bbg-contributors/bbg"
   name "BBG"
-  desc "A static blog generator based on Electron Technology"
+  desc "Static blog generator based on Electron Technology"
   homepage "https://bbg.nekomoe.xyz/"
-
-  # livecheck TODO
 
   auto_updates true
 
   app "Baiyang-lzy's Blog Generator.app"
 
-  zap trash: [
-    "~/Library/Application Support/bbg"
-  ]
+  zap trash: "~/Library/Application Support/bbg"
 end
