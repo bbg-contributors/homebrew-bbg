51b8d970f5c6c1472ae96f1870a7f0d874ef785c
scientificworld <scientificworld@126.com>
2022-12-06 22:44:23 +0800

    Bump version to 20221202

--- Casks/bbg.rb
+++ Casks/bbg.rb
@@ -1,9 +1,9 @@
 cask "bbg" do
   arch arm: "arm64", intel: "x64"
 
-  version "20221130"
-  sha256 arm:   "66375e87d3ff4fd9f54b373a845eeeb31b3d4cdcc341039f5f686a20eb661e78",
-         intel: "c855ed7f4762c8cf6e8196c0e18c144bfd1261bb0f2980e225b5efa5e7d7784a"
+  version "20221202"
+  sha256 arm:   "a2ef50d104011b9552e3bcfc675dff77672961d3846e7f4361f873a648b52d15",
+         intel: "5a66d2b515be1ea567f70bb41af69d6ea563b7281e8029e05a955094223c83fd"
 
   url "https://github.com/bbg-contributors/bbg/releases/download/#{version}/bbg-#{version}-#{arch}.dmg",
       verified: "https://github.com/bbg-contributors/bbg"
