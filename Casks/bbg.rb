bd546305ee7936e0636ae7831090e8a636e2f991
scientificworld <scientificworld@126.com>
2023-10-16 20:29:00 +0800

    bbg 20231016

--- Casks/bbg.rb
+++ Casks/bbg.rb
@@ -1,9 +1,9 @@
 cask "bbg" do
   arch arm: "arm64", intel: "x64"
 
-  version "20230820"
-  sha256 arm:   "97ad732178f22f31758f145467fb679a6796f6c275668979028ff752a4f542f9",
-         intel: "e48aebbc56b4acef721c1b24dd7df8ce2174ee709283a188eb7f8255fea331c3"
+  version "20231016"
+  sha256 arm:   "e21da83540657d0c211f618258dbabdac32ac6353e70413e0901f4b922a396b0",
+         intel: "7bdac7046ef702a7bbf5d785ee352cd80e6051c6c6fa8a6ae5152f56a2bec1e6"
 
   url "https://github.com/bbg-contributors/bbg/releases/download/#{version}/bbg-#{version}-#{arch}.dmg",
       verified: "github.com/bbg-contributors/bbg/"
