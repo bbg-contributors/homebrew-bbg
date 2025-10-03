833b30fd7a274f2a5b59adf683393c72fb26c472
scientificworld <scientificworld@users.noreply.github.com>
2025-10-03 15:13:59 +0800

    bbg 20250929

--- Casks/bbg.rb
+++ Casks/bbg.rb
@@ -1,9 +1,9 @@
 cask "bbg" do
   arch arm: "arm64", intel: "x64"
 
-  version "20250817"
-  sha256 arm:   "47b54c4787ddd83f5ac3ea7fd2720640f053b0cdf1ff3251bf91e61f8fcae4b1",
-         intel: "7bebbc6d87412ac7db0fd0905b54dc25aa9d55b9990eb3439ce58fc620a8eb68"
+  version "20250929"
+  sha256 arm:   "057f455ca766a2002f61f4cd7471b42d26fdb0120f9c2670638f994926e8474f",
+         intel: "f8698269a63f9bb7078412bfa4ec53fcc02de35cdb5abf9d44bc149092073c43"
 
   url "https://github.com/bbg-contributors/bbg/releases/download/#{version}/bbg-#{version}-#{arch}.dmg",
       verified: "github.com/bbg-contributors/bbg/"
